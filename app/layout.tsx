6e461ecd1429137d1450f7e7807efb202acd954e
Peter Choi <jc4883@columbia.edu>
2025-03-01 13:18:40 -0800

    buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,31 @@
+import { cn } from "@/lib/utils";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  preload: true,
+  weight: ["400", "500", "600", "700"],
+  adjustFontFallback: true,
+  variable: "--font-sans",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+"use client";
+
+import { StartCallContent } from "./calls/components/StartCallButton";
+
+const VAPI_AGENT_ID1 = "6e1b2494-d500-4e26-8975-42b4d8985814";
+const VAPI_AGENT_ID2 = "d6840c2a-715f-4b0a-a0e7-13ab2e3770fd";
+
+export default function CallsPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100 p-4">
+      <div className="bg-white shadow-md rounded-lg p-6 m-4 gap-8 flex">
+        <StartCallContent agentId={VAPI_AGENT_ID1} type="bad" />
+        <StartCallContent agentId={VAPI_AGENT_ID2} type="good" />
+      </div>
+    </div>
+  );
 }
